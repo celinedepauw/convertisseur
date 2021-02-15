d03e6d16ae4053926518eb287a227279c6dbfaab
Celine Depauw <celinefauchier@hotmail.com>
2021-02-15 17:26:29 +0100

    composant Result statique ok

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,12 +6,14 @@ import './styles.css';
 
 import Header from 'src/components/Header';
 import Currencies from 'src/components/Currencies';
+import Result from 'src/components/Result';
 
 // == Composant
 const App = () => (
   <div className="app">
     <Header />
     <Currencies />
+    <Result />
   </div>
 );
 
---
+++ src/components/Result/index.js
@@ -0,0 +1,12 @@
+import React from 'react' ;
+
+import './result.scss';
+
+const Result = () => (
+  <div className="result">
+    <h2 className="rate">1.09</h2>
+    <p className="current-currency">United States Dollar</p>
+  </div>
+);
+
+export default Result;
