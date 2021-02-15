0912a35e46efb00662fe247ebe44b1c141249fc3
Celine Depauw <celinefauchier@hotmail.com>
2021-02-15 19:38:33 +0100

    tentative dynamisation currencies avec forEach

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -8,14 +8,25 @@ import Header from 'src/components/Header';
 import Currencies from 'src/components/Currencies';
 import Result from 'src/components/Result';
 
+import allCurrencies from 'src/data/currencies';
+
 // == Composant
-const App = () => (
-  <div className="app">
-    <Header />
-    <Currencies />
-    <Result />
-  </div>
-);
+const App = () => {
+  console.log(allCurrencies);
+  allCurrencies.forEach(function(index) {
+    console.log(index);
+  });
+  return (
+    <div className="app">
+      <Header />
+      <Currencies
+        name={allCurrencies.name}
+        rate={allCurrencies.rate}
+      />
+      <Result />
+    </div>
+  );
+};
 
 // == Export
 export default App;
--- src/components/Currencies/index.js
+++ src/components/Currencies/index.js
@@ -1,22 +1,26 @@
-import React from 'react' ;
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import './currencies.scss';
 
-const Currencies = () => (
+const Currencies = ({ allCurrencies }) => (
   <div className="currencies">
     <h2 className="title">Currencies</h2>
     <ul className="all-currencies">
-      <li className="currency">
-        Australian Dollar
-      </li>
-      <li className="currency">
-        Bulgarian Lev
-      </li>
-      <li className="currency">
-        Brazilian Real
-      </li>
+      <li>Blablabla</li>
     </ul>
   </div>
 );
+
+/*Currencies.propTypes = {
+  // currencies est un tableau d'objets
+  // on précise les différentes propriétés de chaque objet
+  allCurrencies: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      rate: PropTypes.number.isRequired,
+    }).isRequired, // obligatoire qu'il y ait cette "forme"
+  ).isRequired, // la prop ingredients est obligatoire
+};*/
 
 export default Currencies;
