aa3131e90513c8a1f8937929574ee638b30fe3a0
Celine Depauw <celinefauchier@hotmail.com>
2021-02-16 13:39:15 +0100

    changement du state en cliquant sur le bouton

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -18,38 +18,56 @@ Objectif : pouvoir afficher ou masquer les devises grâce à un bouton
 */
 
 // == Composant
-const App = () => {
-  // indique si les devises sont affichées
-  let open = true;
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  constructor(props) {
+    // on appelle le constructeur de React.Component en lui donnant les props
+    super(props);
+    // on créé le state du composant sous forme d'objet
+    this.state = {
+      open: true,
+    };
+  }
 
-  /**
+  render() {
+    /**
    * Affichage conditionnel d'un composant en React
    * {open && <Currencies currencies={currenciesList} />}
    * Si open est false, la suite est ignorée : Currencies ne s'affiche pas
    * Si open est true, la suite est exécutée : Currencies est affiché
    */
 
-  /**
+    /**
     * Liste évènements React
     * https://fr.reactjs.org/docs/events.html
     */
 
-  return (
-    <div className="app">
-      <Header />
-      <button
-        type="button"
-        onClick={() => {
-          console.log('clic');
-          open = false;
-        }}
-      >Toggle Currencies
-      </button>
-      {open && <Currencies currencies={currenciesList} />}
-      <Result />
-    </div>
+    // pour accéder à une information du state, on pourrait noter :
+    // this.state.open
+    // ou :
+    // const { open } = this.state;
 
-  );
+    const { open } = this.state;
+
+    return (
+      <div className="app">
+        <Header />
+        <button
+          type="button"
+          onClick={() => {
+            // je décris les modifs à appliquer sur le state
+            this.setState({
+              // j'inverse la valeur de open (cela fonctionne car open est un booléen)
+              open: !open,
+            });
+          }}
+        >Toggle Currencies
+        </button>
+        {open && <Currencies currencies={currenciesList} />}
+        <Result />
+      </div>
+    );
+  }
 };
 
 // == Export
