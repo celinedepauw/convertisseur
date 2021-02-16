b0eb81c42ab43a4f0ed309da1adc8d9022639c4e
Celine Depauw <celinefauchier@hotmail.com>
2021-02-16 15:36:25 +0100

    creation d'un composant CustomButton dynamique

--- src/components/Result/index.js
+++ src/components/Result/index.js
@@ -1,11 +1,11 @@
-import React from 'react' ;
+import React from 'react';
 
 import './result.scss';
 
 const Result = () => (
   <div className="result">
     <h2 className="rate">1.09</h2>
-    <p className="current-currency">United States Dollar</p>
+    <p className="current-currency">United USD</p>
   </div>
 );
 
